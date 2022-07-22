bccdbf92bf8029ebc3a8f760fbd921ca3d6388aa
nik2704 <nik2704@yandex.ru>
2022-07-22 13:57:11 +0300

    22.07.2022 Commit-2

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,57 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.util.concurrent.Callable;
+
+import static picocli.CommandLine.Help.Visibility.NEVER;
+
+
+//public class App implements Callable<Integer> {
+@Command(name = "gendiff",
+        description="Compares two configuration files and shows a difference.",
+        headerHeading="%n")
 public class App {
+    private final String OUTPUT_FORMAT = "stylish";
+
+    @Option(names = {"-f", "--format"},
+            paramLabel = "format",
+            description = "output format [default: " + OUTPUT_FORMAT +"]",
+            showDefaultValue = NEVER,
+            defaultValue = OUTPUT_FORMAT)
+    String format;
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    boolean usageHelpRequested;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    boolean versionInfoRequested;
+
+    @Parameters(paramLabel="filepath1", description = "path to first file")
+    File filepath1;
+
+    @Parameters(paramLabel="filepath2", description = "path to second file")
+    File filepath2;
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+//        int exitCode = new CommandLine(new App()).execute(args);
+        CommandLine commandLine = new CommandLine(new App());
+        commandLine.parseArgs(args);
+        if (commandLine.isUsageHelpRequested()) {
+            commandLine.usage(System.out);
+            return;
+        } else if (commandLine.isVersionHelpRequested()) {
+            commandLine.printVersionHelp(System.out);
+            return;
+        }
+//        System.exit(exitCode);
     }
+
+//    @Override
+//    public Integer call() throws Exception {
+//        return null;
+//    }
 }
